55086c13678a1be1f9ef829bd1a5008a49b475a9
Mateusz Harcej <c16344691@mytudublin.ie>
2023-03-20 00:21:20 +0000

    maxscale setup

--- primaryinit/primaryinit.sql
+++ primaryinit/primaryinit.sql
@@ -1,5 +1,6 @@
 CREATE USER 'replication_user'@'%' IDENTIFIED BY 'replsecret';
 GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%';
 
-CREATE USER 'maxscale'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON *.* TO 'maxscale'@'%';
+CREATE USER maxscale@'%' IDENTIFIED BY 'password';
+GRANT SELECT ON mysql.* TO maxscale@'%';
+GRANT SHOW DATABASES, SLAVE MONITOR ON *.* TO maxscale@'%';
